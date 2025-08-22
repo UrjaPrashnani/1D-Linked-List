fdbfdad3f47f8a575971ad0d93d7abbe3c0d0423
UrjaPrashnani <urjaprashnani@gmail.com>
2025-08-22 20:42:20 +0530

    dsa

---
+++ length_of_LL.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public: // access modifier
+    int data; // the data value
+    Node* next; // the pointer to the next value
+   
+    // constructor
+    Node (int data1, Node* next1){
+        data=data1;  // Initialize data with the provided value
+        next=next1;  // Initialize next with the provided
+    }
+    Node (int data1){
+        data=data1;  // Initialize data with the provided value
+        next=nullptr;  // Initialize next as null since it's the end of the list
+
+    }
+};
+Node* Convert(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+int length(Node* head){
+    int c=0;
+    Node* temp=head;
+    while(temp){
+        temp=temp->next;
+        c++;
+    }
+    return c;
+}
+int main(){
+    vector<int> arr={2,5,8,7};
+    Node* head= Convert(arr);
+    cout<<length(head);
+}
