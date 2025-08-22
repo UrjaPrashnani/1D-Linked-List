f907ac8920193235c884311a6d80ee185d05dcca
UrjaPrashnani <urjaprashnani@gmail.com>
2025-08-22 20:47:19 +0530

    dsa

---
+++ search_in_LL.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public: // access modifier
+    int data; // the data value
+    Node* next; // the pointer to the next value
+   
+    // constructor
+    Node (int data1, Node* next1){
+        data=data1;  // Initialize data with the provided value
+        next=next1;  // Initialize next with the provided
+    }
+    Node (int data1){
+        data=data1;  // Initialize data with the provided value
+        next=nullptr;  // Initialize next as null since it's the end of the list
+
+    }
+};
+Node* Convert(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+int search(Node* head,int val){
+    
+    Node* temp=head;
+    while(temp){
+        if(temp->data==val) return 1;
+        temp=temp->next;
+    }
+    return 0;
+}
+int main(){
+    vector<int> arr={2,5,8,7};
+    Node* head= Convert(arr);
+    cout<<search(head , 9);
+}
